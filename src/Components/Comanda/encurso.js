3c26f28a91cbde29a6c6bda37f40693884d70fff
daviddflix <david-972010@hotmail.com>
2022-09-12 18:25:17 -0300

    merging

--- src/Components/Comanda/encurso.js
+++ src/Components/Comanda/encurso.js
@@ -176,3 +176,97 @@ function CardMultiple({image, method, color, alt}){
         </div>
     )
 } 
+
+
+function CardMethod({ image, method, color, alt }) {
+
+    const { newCart, setNewCart } = useContext(cartContext)
+    const handleChange = (e) => {
+      setNewCart({ ...newCart, method: method });
+    };
+  
+    return (
+      <div onClick={handleChange}  style={{ backgroundColor: color }} className={s.containerMethod}>
+        <img className={s.imageMethod} src={image} alt={alt} />
+        <h3 style={{textTransform: 'uppercase'}}>{method}</h3>
+      </div>
+    )
+  } 
+  
+  
+  function Multiples() {
+    const [open, setOpen] = React.useState(false);
+    const [fullWidth] = React.useState(true);
+    const [maxWidth] = React.useState('sm');
+  
+    const handleClickOpen = () => {
+      setOpen(true);
+    };
+  
+    const handleClose = () => {
+      setOpen(false);
+    };
+  
+    const { newCart, setNewCart } = useContext(cartContext);
+   
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setNewCart(prev => ({ ...prev, multiple: {...prev.multiple, [name]: value} }));
+      };
+  
+      const setMethod = () => {
+        setNewCart(prev => ({ ...prev, method: 'Varios' }));
+      }
+  
+    return (
+      <React.Fragment>
+        <Button onClickCapture={setMethod} variant="outlined" style={{ fontSize: '1rem',fontWeight: 800, backgroundColor: newCart.method === 'Varios'? '#009ee3' : '#fff', border: 'none', color: '#282828'}} size="small" className={s.containerMethod} onClick={handleClickOpen}>
+         <img className={m.iconsMethod} alt="varios" src='https://png.pngtree.com/png-vector/20190804/ourmid/pngtree-payment-bank-banking-card-credit-mobile-money-smartphone-png-image_1650511.jpg'/>
+         Multiples medios
+        </Button>
+        <Dialog
+          fullWidth={fullWidth}
+          maxWidth={maxWidth}
+          open={open}
+          onClose={handleClose}
+        >
+          {/* <DialogTitle>Elige una forma de cobro</DialogTitle> */}
+          <DialogContent>
+            <Box
+              noValidate
+              component="form"
+              sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                m: 'auto',
+                width: 'fit-content',
+              }}
+            >
+              <div className={m.mainContainerMethod}>
+                  <div style={{backgroundColor: '#fff'}}  className={m.containerMethodCard}>
+                    <div className={m.boxImage}>
+                    <input className={m.iconsMethod} type='image' src={'https://img.utdstc.com/icon/f24/b94/f24b94db83f2c097744c62d36981fd056214096b5adb5ae80d651d188579af1e:200'} alt={'Efectivo'} />
+                    <h3 style={{fontSize: '1rem', textTransform: 'uppercase'}}>Efectivo</h3>
+                    </div>
+                    <TextField type='number' value={newCart.multiple.Efectivo || ''} onChange={handleChange} name={'Efectivo'}  className={m.textfield} id="filled-basic" label="Ingresa un monto" variant="filled" />
+                  </div>
+                  <div  style={{backgroundColor: '#fff'}}  className={m.containerMethodCard}>
+                    <div className={m.boxImage}>
+                    <input className={m.iconsMethod} type='image' src={'https://static.vecteezy.com/system/resources/previews/004/996/077/original/qr-code-scanning-qr-code-reader-app-concept-icon-recognition-or-reading-qr-code-in-flat-style-green-and-blue-scanner-application-line-icon-illustration-vector.jpg'} alt={'QR'} />
+                    <h3 style={{fontSize: '1rem', textTransform: 'uppercase'}}>QR</h3>
+                    </div>
+                    <TextField type='number' value={newCart.multiple.QR || ''} onChange={handleChange} name={'QR'}  className={m.textfield} id="filled-basic" label="Ingresa un monto" variant="filled" />
+                  </div>
+              </div>
+  
+            </Box>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleClose}>cancelar</Button>
+            <Button onClick={handleClose}>confirmar</Button>
+          </DialogActions>
+        </Dialog>
+      </React.Fragment>
+    );
+  }
+  
